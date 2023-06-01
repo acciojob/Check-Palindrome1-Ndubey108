3d30a979f43ff12a5ff789dac218e29cb295c88d
Nidhi Dubey <93912780+Ndubey108@users.noreply.github.com>
2023-06-01 12:37:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // complete the given function
 
 function palindrome(str){
-
+str = str.split("");
+	while(str.length){
+		if(str[0] === str[str.length-1]){
+		str.shift();
+			str.pop()
+	}
+	else{
+	    return false
+	}
+	}
+	if(str.length === 0){
+	    return true
+	}
+	
 }
 module.exports = palindrome
